d7b318a7ba6865260b62d23c13a05f73ac315c97
Shivoo29 <2004skj@gmail.com>
2026-06-24 01:32:52 +0530

    chore: update API_BASE defaults to Render URL

--- solvor_tutor/lib/modules/onboarding/presentation/screens/otp_verification_screen.dart
+++ solvor_tutor/lib/modules/onboarding/presentation/screens/otp_verification_screen.dart
@@ -10,7 +10,7 @@ import '../../../../core/l10n/strings_provider.dart';
 import '../onboarding_provider.dart';
 import '../widgets/step_progress.dart';
 
-const _apiBase = String.fromEnvironment('API_BASE', defaultValue: 'https://solvor-backend.up.railway.app');
+const _apiBase = String.fromEnvironment('API_BASE', defaultValue: 'https://solvor-tutor.onrender.com');
 
 class OtpVerificationScreen extends ConsumerStatefulWidget {
   const OtpVerificationScreen({super.key});
